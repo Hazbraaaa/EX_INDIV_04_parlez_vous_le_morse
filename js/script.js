87b30ad50e7c7fcfef27edd4477f12d51a9184ff
chazera <hazera.c@gmail.com>
2025-03-10 17:24:21 +0100

    etape 2

---
+++ js/script.js
@@ -0,0 +1,40 @@
+const latinToMorse = {
+	'A':'.-',
+	'B':'-...',
+	'C':'-.-.',
+	'D':'-..',
+	'E':'.',
+	'F':'..-.',
+	'G':'--.',
+	'H':'....',
+	'I':'..',
+	'J':'.---',
+	'K':'-.-',
+	'L':'.-..',
+	'M':'--',
+	'N':'-.',
+	'O':'---',
+	'P':'.--.',
+	'Q':'--.-',
+	'R':'.-.',
+	'S':'...',
+	'T':'-',
+	'U':'..-',
+	'V':'...-',
+	'W':'.--',
+	'X':'-..-',
+	'Y':'-.--',
+	'Z':'--..'
+};
+
+function getLatinCharacterList(string) {
+    return string.split("");
+}
+
+function translateLatinCharacter(character) {
+    for (const char in latinToMorse) {
+        if (char === character) {
+            return latinToMorse[char];
+        }
+    }
+}
