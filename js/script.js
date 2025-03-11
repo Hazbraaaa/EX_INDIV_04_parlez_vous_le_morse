59fc2845ea1594819f2c0bec18a506b1622067f4
chazera <hazera.c@gmail.com>
2025-03-11 16:37:17 +0100

    finished

--- js/script.js
+++ js/script.js
@@ -1,34 +1,66 @@
 const latinToMorse = {
-	'A':'.-',
-	'B':'-...',
-	'C':'-.-.',
-	'D':'-..',
-	'E':'.',
-	'F':'..-.',
-	'G':'--.',
-	'H':'....',
-	'I':'..',
-	'J':'.---',
-	'K':'-.-',
-	'L':'.-..',
-	'M':'--',
-	'N':'-.',
-	'O':'---',
-	'P':'.--.',
-	'Q':'--.-',
-	'R':'.-.',
-	'S':'...',
-	'T':'-',
-	'U':'..-',
-	'V':'...-',
-	'W':'.--',
-	'X':'-..-',
-	'Y':'-.--',
-	'Z':'--..'
+    'A': '.-',
+    'B': '-...',
+    'C': '-.-.',
+    'D': '-..',
+    'E': '.',
+    'F': '..-.',
+    'G': '--.',
+    'H': '....',
+    'I': '..',
+    'J': '.---',
+    'K': '-.-',
+    'L': '.-..',
+    'M': '--',
+    'N': '-.',
+    'O': '---',
+    'P': '.--.',
+    'Q': '--.-',
+    'R': '.-.',
+    'S': '...',
+    'T': '-',
+    'U': '..-',
+    'V': '...-',
+    'W': '.--',
+    'X': '-..-',
+    'Y': '-.--',
+    'Z': '--..'
+};
+const morseToLatin = {
+    '-': "T",
+    '--': "M",
+    '---': "O",
+    '--.': "G",
+    '--.-': "Q",
+    '--..': "Z",
+    '-.': "N",
+    '-.-': "K",
+    '-.--': "Y",
+    '-.-.': "C",
+    '-..': "D",
+    '-..-': "X",
+    '-...': "B",
+    '.': "E",
+    '.-': "A",
+    '.--': "W",
+    '.---': "J",
+    '.--.': "P",
+    '.-.': "R",
+    '.-..': "L",
+    '..': "I",
+    '..-': "U",
+    '..-.': "F",
+    '...': "S",
+    '...-': "V",
+    '....': "H"
 };
 
 function getLatinCharacterList(string) {
     return string.split("");
+}
+
+function getMorseCharacterList(string) {
+    return string.split(" ");
 }
 
 function translateLatinCharacter(character) {
@@ -37,14 +69,33 @@ function translateLatinCharacter(character) {
             return latinToMorse[char];
         }
     }
+    return "/";
 }
+
+function translateMorseCharacter(character) {
+    for (const char in morseToLatin) {
+        if (char === character) {
+            return morseToLatin[char];
+        }
+    }
+    return " ";
+}
+
 function encode(string) {
     const arrayOfString = getLatinCharacterList(string);
 
     arrayOfString.forEach((char) => {
         console.log(translateLatinCharacter(char));
-        
-    })
+    });
 }
 
-encode("HELLO");
+function decode(string) {
+    const arrayOfString = getMorseCharacterList(string);
+
+    arrayOfString.forEach((char) => {
+        console.log(translateMorseCharacter(char));
+    });
+}
+
+encode("HELLO WORLD");
+decode(".... . .-.. .-.. --- / .-- --- .-. .-.. -..");
